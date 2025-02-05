833c0bf537a6736ae8782c76927ee978315f9020
Man-Q <81616738+Man-Q@users.noreply.github.com>
2025-02-05 20:17:31 +0200

    Personal info input fields

---
+++ src/app/(main)/billing/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <main>Billing</main>
+  )
+}
+
+export default Page
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Your resumes",
+};
 
 const page = () => {
   return (
-    <div>
-      Resumes Coming...
-    </div>
-  )
-}
+    <main className="max-w-7xl mx-auto w-full px-3 py-6 space-y-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href="/editor">
+          <PlusSquare className="size-5" />
+          New resume
+        </Link>
+      </Button>
+    </main>
+  );
+};
 
-export default page
+export default page;
