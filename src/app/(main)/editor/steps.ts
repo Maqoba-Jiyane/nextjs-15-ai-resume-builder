dc693c218972d083b6b4bff8b06dc05cda360efd
Man-Q <81616738+Man-Q@users.noreply.github.com>
2025-02-06 18:12:45 +0200

    work experience

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,6 +1,7 @@
 import { EditorFormProps } from "@/lib/types"
 import GeneralInfoForm from "./forms/GeneralInfoForm"
 import PersonalInfoForm from "./forms/PersonalInfoForm"
+import WorkExperienceForm from "./forms/WorkExperienceForm"
 
 
 export const steps: {
@@ -14,5 +15,8 @@ export const steps: {
     },
     {
         title: "Personal info", component: PersonalInfoForm, key: "personal-info"
+    },
+    {
+        title: "Work experience", component: WorkExperienceForm, key: "work-experience"
     }
 ]
