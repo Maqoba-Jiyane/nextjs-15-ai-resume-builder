3c78c1a1e56a8c58391c03d5190ce1a76d40f06c
Man-Q <81616738+Man-Q@users.noreply.github.com>
2025-02-11 14:24:11 +0200

    fixing mobile print

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -28,7 +28,7 @@ const ResumePreview = ({ resumeData, contentRef, className }: ResumePreviewProps
       <div
         className={cn("space-y-6 p-6", !width && "invisible")}
         style={{
-          zoom: 1,
+          zoom: (1 / 794) * width,
         }}
         ref={contentRef}
         id="resumePreviewContent"
