aca9ca55a59009e20d3459fbe913010cc25e1290
Man-Q <81616738+Man-Q@users.noreply.github.com>
2025-02-19 10:51:32 +0200

    Renamings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: {
     template: `%s - EonResume`,
-    absolute: "AI Resume Builder",
+    absolute: "EonResume",
   },
   description:
     "EonResume is the easiest way to create a professional resume to help you lend your dream job.",
