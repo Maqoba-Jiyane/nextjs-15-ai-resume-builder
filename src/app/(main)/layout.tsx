302d1d47c25986a74051de05f24f28b856eaa1f1
Man-Q <81616738+Man-Q@users.noreply.github.com>
2025-03-26 08:38:14 +0200

    how-to-tailor-your-resume-for-jobs

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
 // import PremiumModal from "@/components/premium/PremiumModal";
-
+export const metadata = {
+  facebook: {
+    appId: '23449',
+  },
+}
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
